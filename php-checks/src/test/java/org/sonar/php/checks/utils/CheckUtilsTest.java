5eecd7e4f8c1c1cf5c6540716289595b4054fef0
Alban Auzeill <alban.auzeill@sonarsource.com>
2017-11-10 15:19:54 +0100

    Fix quality flaw

--- php-checks/src/test/java/org/sonar/php/checks/utils/CheckUtilsTest.java
+++ php-checks/src/test/java/org/sonar/php/checks/utils/CheckUtilsTest.java
@@ -20,6 +20,7 @@
 package org.sonar.php.checks.utils;
 
 import com.sonar.sslr.api.typed.ActionParser;
+import com.sonarsource.checks.coverage.UtilityClass;
 import org.junit.Test;
 import org.sonar.php.parser.PHPLexicalGrammar;
 import org.sonar.php.parser.PHPParserBuilder;
@@ -35,6 +36,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CheckUtilsTest {
 
   private ActionParser<Tree> parser = PHPParserBuilder.createParser(PHPLexicalGrammar.TOP_STATEMENT);
+
+  @Test
+  public void utility_class() throws Exception {
+    UtilityClass.assertGoodPractice(CheckUtils.class);
+  }
 
   @Test
   public void skipParenthesis() throws Exception {
